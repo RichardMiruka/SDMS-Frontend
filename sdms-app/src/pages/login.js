f84265df6d93d8f210d46fe2e831b0881f7c0dd5
RichardMiruka <richardonsare15@gmail.com>
2024-01-29 15:51:32 +0300

    created our login page

---
+++ sdms-app/src/pages/login.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import './login.css';
+import { useNavigate, Link } from 'react-router-dom'
+
+const LoginPage = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError]= useState('');
+
+  const navigate=useNavigate()
+  const handleLogin=async()=>{
+    try { const response=await fetch('https://loginapiurl/api/v1/Login',{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify({username, password}),
+  }
+  );
+  const data=await response.json()
+  console.log('Login Response Data:', data['access-token']);
+  if (response.ok){
+    localStorage.setItem("token",data['access-token'])
+    navigate('/store')
+  }
+  else{
+    setError(data.error)
+  }  
+    } catch (error) {
+      setError('An error occurred, please do try again')
+    }
+  }
+
+
+  return (
+    <div>
+    <img src="client/src/images/ourimage.png" alt="logo"/>
+    {error && <p>{error}</p>}
+      <form>
+        <h1>Log in</h1>
+        <label>Username</label>
+        <input
+          type='text'
+          placeholder='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <label>Password</label>
+        <input
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type='button' onClick={handleLogin}>Login</button>
+        <br />
+        <Link to="/register">Don't have an account?</Link>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
