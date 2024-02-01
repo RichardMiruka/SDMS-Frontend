1bb38c81b48323946287998a2051faa71b9dfbe7
RichardMiruka <richardonsare15@gmail.com>
2024-02-01 16:46:42 +0300

    made changes to my pages

--- sdms-app/src/pages/Home.js
+++ sdms-app/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome to the Tournament Site</h2>
-      <p>Explore tournaments, view match results, and enjoy the competition.</p>
+    <div className="jumbotron">
+      <h1 classname="display-4"><b>Welcome to the Tournament Site</b></h1>
+      <br/>
+      <p className="lead">This is a simple tournament site that allows you to explore tournaments, view match results, and enjoy the competition.</p>
+      {/* <hr className="my-4" /> */}
+      <p classname='lead'>This platform allows Event Organizers to create, update and manage tournament fixtures</p><br/>
+      <p classname="lead">
+        <Link className="btn btn-primary btn-lg" to="/tournaments" role="button">View Tournaments</Link>
+      </p>
     </div>
   );
 };
