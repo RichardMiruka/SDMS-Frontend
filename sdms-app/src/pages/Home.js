582104206ce4b6acac9ca114b5832f12144fe3a8
RichardMiruka <richardonsare15@gmail.com>
2024-02-01 12:40:23 +0300

    added Home component

---
+++ sdms-app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the Tournament Site</h2>
+      <p>Explore tournaments, view match results, and enjoy the competition.</p>
+    </div>
+  );
+};
+
+export default Home;
--- sdms-app/src/pages/TournamentPage.js
+++ sdms-app/src/pages/TournamentPage.js
@@ -66,4 +66,4 @@ const SingleEliminationTournament = ({ numPlayers }) => {
   );
 };
 
-export default EliminationTournament;
+export default SingleEliminationTournament;
