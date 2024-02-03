d432a6c8d38e9c13dbcc76ba200299f964cff1ac
Abokiey <sweneysam@gmail.com>
2024-02-03 16:56:09 +0300

    changes before starting

--- sdms-app/src/App.js
+++ sdms-app/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import TournamentPage from './pages/TournamentPage';
-import './App.css';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import TournamentPage from './pages/TournamentPage';
+// import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/tournament" element={<TournamentPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <h1>Hello world</h1>
+    // <Router>
+    //   <div>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //       <Route path="/tournament" element={<TournamentPage />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 };
 
