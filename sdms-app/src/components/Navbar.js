f10494a1400cc9052a421c2c671a814a117e9e28
Abokiey <sweneysam@gmail.com>
2024-02-10 20:47:04 +0300

    navbar

--- sdms-app/src/components/Navbar.js
+++ sdms-app/src/components/Navbar.js
@@ -67,22 +67,3 @@ const Navbar = () => {
 export default Navbar;
 
 
-// const Navbar = () => {
-//   return (
-//     <nav className='bg-blue-700 text-white p-4'>
-//       <div className="container mx-auto flex justify-between text-center">
-//         <div className="text-xl text-bold">SDMS</div>
-//         <ul className='flex space-x-4'>
-//           <li><Link to="/">Home</Link></li>
-//           <li><Link to="/dashboard">Dashboard</Link></li>
-//           <li><Link to="/about">About</Link></li>
-//           <li><Link to="/contact">Contact</Link></li>
-//           <li><Link to="/tournament">Tournament</Link></li>
-//           <li><Link to="/Register">Register</Link></li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
