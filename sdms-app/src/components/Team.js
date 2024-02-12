2a8a82a65dc6fe8840fe591f8c6d595f7d4656b6
Abokiey <sweneysam@gmail.com>
2024-02-12 17:38:58 +0300

    team photo

--- sdms-app/src/components/Team.js
+++ sdms-app/src/components/Team.js
@@ -27,7 +27,7 @@ import {
 
    
   const TABLE_HEAD = ["Team", "Coach", "status", "Date of registration", ""];
-  const defaultTeamImg = "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg"
+  const defaultTeamImg = "https://images.unsplash.com/photo-1631495634750-0f14320bc0a7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
    
 
   export function TeamList() {
