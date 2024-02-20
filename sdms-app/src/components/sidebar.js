e8c366136f9c70d8081ee819997a658308b3aa81
Abokiey <sweneysam@gmail.com>
2024-02-20 14:53:51 +0300

    rem sidebar

--- sdms-app/src/components/sidebar.js
+++ sdms-app/src/components/sidebar.js
@@ -3,26 +3,6 @@
 
 // function Sidebar() {
 
-<<<<<<< HEAD
-  return (
-    <div className='flex'>
-      <div className=' flex bg-gray-500 h-screen w-30 p text-white text-center'>
-          <div className='space-y-20'>
-                <div>Sidebar</div>
-                <ul className='flex-frow space-y-6 p-4'>
-                    <li><Link to='event'>Games</Link></li>
-                    <li><Link to="Team">Team</Link></li>
-
-                    <li><Link to="coaches">Coaches</Link></li>
-
-                    <li><Link to="players">Players</Link></li>
-                </ul>
-            </div>
-      </div>
-      <div className='mr-4 ml-8'>
-        <Outlet />
-      </div>
-=======
 //   return (
 //     <div className='flex'>
 //       <div className=' flex bg-gray-500 h-screen w-40 p text-white text-center'>
@@ -39,7 +19,6 @@
 //       <div className='mr-4 ml-8'>
 //         <Outlet />
 //       </div>
->>>>>>> aboki
       
 //     </div>
 //   )
