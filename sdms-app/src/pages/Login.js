8627103d38adaef0ec9305fe184ff20801d931e1
RichardMiruka <richardonsare15@gmail.com>
2024-02-21 13:55:29 +0300

    did styling to our Loginpage

--- sdms-app/src/pages/Login.js
+++ sdms-app/src/pages/Login.js
@@ -8,7 +8,6 @@ const LoginPage = ({ onLogin }) => {
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
-
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
@@ -43,71 +42,82 @@ const LoginPage = ({ onLogin }) => {
   }, []);
 
   return (
-    <div className="bg-gradient-to-r from-gray-300 via-green-200 to-indigo-300 flex justify-center items-center h-screen">
-      <div className="w-full max-w-md bg-gradient-to-r from-blue-500 via-purple-400 to-purple-700 rounded-xl p-8">
-        {message && (
-          <p className="bg-green-500 shadow-md text-center text-white p-2 rounded-md mt-4">
-            {message}
-          </p>
-        )}
-        {error && (
-          <p className="bg-red-500 shadow-md text-center text-white p-2 rounded-md mt-4">
-            {error}
-          </p>
-        )}
-        <form onSubmit={handleLogin}>
-          <h1 className="text-center text-2xl font-bold mb-6">Log in</h1>
-          <div className="mb-4">
-            <label htmlFor="email" className="text-white">
-              Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              ref={mailRef}
-              placeholder="Email"
-              className="w-full p-2 rounded-md shadow-md"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="text-white">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              ref={pwdRef}
-              className="w-full p-2 rounded-md shadow-md"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="flex items-center justify-between mb-4">
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" className="rounded-sm" />
-              <span className="text-white">Remember me</span>
+    <div className="flex h-screen">
+      <div className="w-1/2 bg-gradient-to-r from-gray-300 via-green-200 to-indigo-300 p-8">
+        {/* Your image */}
+        <img
+          src="/images/Tournament%20Brackets.jpg"
+          alt="Tournament Brackets"
+          className="object-cover w-full h-full rounded-xl"
+        />
+      </div>
+      <div className="w-1/2 flex justify-center items-center">
+        <div className="w-full max-w-md bg-gradient-to-r from-blue-500 via-purple-400 to-purple-700 rounded-xl p-8">
+          {message && (
+            <p className="bg-green-500 shadow-md text-center text-white p-2 rounded-md mt-4">
+              {message}
+            </p>
+          )}
+          {error && (
+            <p className="bg-red-500 shadow-md text-center text-white p-2 rounded-md mt-4">
+              {error}
+            </p>
+          )}
+          <form onSubmit={handleLogin}>
+            <h1 className="text-center text-2xl font-bold mb-6">Log in</h1>
+            <div className="mb-4">
+              <label htmlFor="email" className="text-white">
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                ref={mailRef}
+                placeholder="Email"
+                className="w-full p-2 rounded-md shadow-md"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <button
-              type="submit"
-              onClick={handleLogin}
-              className="bg-blue-700 transition-all duration-300 hover:bg-blue-500 text-white px-8 py-2 rounded-md"
-            >
-              Login
-            </button>
-          </div>
-          <div className="text-center text-white">
-            Don't have an account?{" "}
-            <span className="text-blue-700 underline font-bold text-md">
-              <Link to="/register">Sign Up</Link>
-            </span>
-          </div>
-        </form>
+            <div className="mb-4">
+              <label htmlFor="password" className="text-white">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                ref={pwdRef}
+                className="w-full p-2 rounded-md shadow-md"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between mb-4">
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" className="rounded-sm" />
+                <span className="text-white">Remember me</span>
+              </div>
+              <button
+                type="submit"
+                onClick={handleLogin}
+                className="bg-blue-700 transition-all duration-300 hover:bg-blue-500 text-white px-8 py-2 rounded-md"
+              >
+                Login
+              </button>
+            </div>
+            <div className="text-center text-white">
+              Don't have an account?{" "}
+              <span className="text-blue-700 underline font-bold text-md">
+                <Link to="/register">Sign Up</Link>
+              </span>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
 };
 
 export default LoginPage;
+
